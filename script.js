5a5444bf85417626138e4ee0c537773ec8c6a2fa
arshad031 <arshad.cse.sub48@gmail.com>
2025-07-29 21:54:23 +0600

    simple todo app with html css and javascript

---
+++ script.js
@@ -0,0 +1,49 @@
+const todoForm = document.getElementById("todo-form");
+const todoInput = document.getElementById("todo-input");
+const todoList = document.getElementById("todo-list");
+
+let todos = [];
+
+function renderTodos() {
+  todoList.innerHTML = '';
+  todos.forEach((todo, index) => {
+    const li = document.createElement('li');
+    li.className = "list-group-item d-flex justify-content-between align-items-center ";
+    li.textContent = todo;
+
+    const deleteBtn = document.createElement("button");
+    deleteBtn.className = "btn btn-danger btn-sm";
+    deleteBtn.textContent = "Delete";
+
+    deleteBtn.addEventListener("click", () => {
+      deleteTodo(index);
+    });
+
+    li.appendChild(deleteBtn);
+    todoList.appendChild(li);
+	
+	const updateBtn = document.createElement("button");
+    updateBtn.className = "btn btn-success btn-sm me-2";
+    updateBtn.textContent = "Update";
+
+  });
+}
+
+function addTodo(todoText) {
+  if (todoText.trim() === '') return;
+
+  todos.push(todoText.trim());
+  renderTodos();
+  todoInput.value = '';
+}
+
+todoForm.addEventListener('submit', function(e) {
+  e.preventDefault();
+  addTodo(todoInput.value);
+});
+
+function deleteTodo(index) {
+  todos.splice(index, 1);
+  renderTodos();
+}
+
